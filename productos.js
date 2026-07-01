fe75739192d47827e0b1091fe6d62256689dd19f
insumoscreativos <motorcobrainc@gmail.com>
2026-07-01 16:51:20 -0400

    Add files via upload

--- productos.js
+++ productos.js
@@ -1,36 +1,106 @@
 const myProducts = [
-
-            { 
-                sku: "TICA0", 
-                name: "Lanyard Porta-Celular corto", 
-                price: 790, 
-                desc: "Lanyard Porta-Celular corto de poliéster liso negro, con terminación de eco-cuero negro + mosquetón metálico + placa porta-celular de PVC negro.<br><br><strong>Tamaño:</strong> Lanyard: largo total 86 cm (doble * 43 cm) / ancho: 2cm <br><strong>Placa:</strong> 6 x 4 cm / Largo total: 54.7 cm.<br><strong>Peso:</strong> 10g.<br><strong>Colores:</strong> Negro.<br><strong>Sugerencia de Impresión:</strong> Serigrafía sobre el lanyard de poliéster.<br><strong>Ancho Lanyard:</strong> 2 cm.<br><strong>Material:</strong> Poliéster, PU, Acero Inoxidable<br><br><strong class='text-red-600'>𝐌𝐢́𝐧𝐢𝐦𝐨 𝟏𝟐 𝐮𝐧𝐢𝐝𝐚𝐝𝐞𝐬.</strong>", 
-                category: ["LANYARDS E IDENTIFICACIÓN", "ACCESORIOS CELULAR"], 
-                images: ["A0-001.jpg", "A0-002.jpg", "A0-003.jpg", "A0-004.jpg", "A0-005.jpg", "A0-006.jpg"] 
-            },
-            { 
-                sku: "TICH48", 
-                name: "Brasero / Parrilla para Carbón o Leña", 
-                price: 74900, 
-                desc: "Brasero / Parrilla para Carbón o Leña, de Acero negro.<br><br><strong>Altura para cocinar:</strong> 31cm.<br><strong>Rejilla cromada:</strong> 51 x 51 x 4mm.<br><strong>Rejilla para carbón:</strong> 34 x 34cm x 3.5mm.<br><strong>Tapa con rejilla:</strong> 52 x 52 x 8.5cm.<br><strong>Bowl para fuego:</strong> 57 x 57 x 11.5cm.<br><strong>Patas:</strong> 7 x 7 x 29.5cm.<br><strong>Tamaño total:</strong> 81 x 81 x 44 cm.<br><strong>Peso:</strong> 12.5 kg.<br><strong>Material:</strong> Acero inoxidable.<br><strong>Sugerencia:</strong> Grabado Láser.<br><br>Incluye Manual instructivo de armado en español.", 
-                category: ["DESTACADOS", "INVIERNO"], 
-                images: ["H48-1.jpeg", "H48-2.jpeg", "H48-3.jpeg", "H48-4.jpeg"] 
-            },
-            { 
-                sku: "TICH49", 
-                name: "Chimenea de Exterior para Carbón o Leña", 
-                price: 49900, 
-                desc: "Chimenea de Exterior, para Carbón o Leña, de Acero negro. Con 3 compartimentos: inferior para almacenaje de leña o carbón, medio para hacer fuego, superior para tiraje de humo. Fácil de armar y transportar.<br><br><strong>Tamaño:</strong> 40 x 40 x 120 cm.<br><strong>Peso:</strong> 10.4 kg.<br><strong>Material:</strong> Acero inoxidable.<br><strong>Sugerencia:</strong> Grabado Láser.<br><br>Incluye Manual instructivo de armado en español.", 
-                category: ["DESTACADOS", "INVIERNO"], 
-                images:["H49-1-900x900.jpg","H49-2-900x900.jpg","H49-3-900x900.jpg","H49-4-900x900.jpg"] 
-            },
-            { 
-                sku: "TICH39", 
-                name: "Chimenea de Exterior para Carbón o Leña", 
-                price: 79900, 
-                desc: "Chimenea de Exterior, para Carbón o Leña, de Acero negro. Con 3 compartimentos: inferior para almacenaje de leña o carbón, medio para hacer fuego, superior para tiraje de humo. Fácil de armar y transportar.<br><br><strong>Tamaño:</strong> Diámetro 45 x H 134 cm.<br><strong>Peso:</strong> 9.3 kg.<br><strong>Material:</strong> Acero inoxidable.<br><strong>Color:</strong> Negro.<br><br>Fácil de armar y transportar.", 
-                category: ["DESTACADOS", "INVIERNO"], 
-                images: ["H39-01.jpg", "H39-02.jpg", "H39-03.jpg"] 
-            }
+  {
+    "sku": "TICH52",
+    "name": "Alfombra reflectante anti-flama",
+    "price": 21900,
+    "desc": "¿Quieres evitar que las chispas y brasas de tu parrilla o brasero dañen tu piso? Te presentamos esta espectacular alfombra anti-flama de tela de Silicona Fibra de Vidrio, con borde de tela reflectante para ver en la oscuridad. Resiste temperaturas hasta 1.000°C (2.000°F), gracias a su capacidad de reflexión de calor del 96%. Contiene 4 ojetillos en los vértices, para fijar al pasto o tierra. Apilable, liviana, lavable y reutilizable.\n\nTamaño: 1 x 1 m.\nPeso: 500g\nColores: Plateado\nSugerencia de Impresión: No apto para impresión\nMaterial: Tela de Silicona Fibra de Vidrio, Tela reflectante.",
+    "category": [
+      "DESTACADOS",
+      "INVIERNO"
+    ],
+    "images": [
+      "H52-01.jpg",
+      "H52-02.jpg",
+      "H52-03.jpg",
+      "H52-04.jpg",
+      "H52-05.jpg"
+    ]
+  },
+  {
+    "sku": "TICM31",
+    "name": "Botella Plástica deportiva de 700cc",
+    "price": 1000,
+    "desc": "Botella Plástica deportiva de 700cc. Sólo para líquidos fríos. Contiene una cintura ideal para la mayoría de los porta-botellas de bicicletas. Boquilla ajustable de silicona negra, que evita la filtración de líquido y entrega mayor duración de uso. Material plástico reciclado LDPE, libre de BPA. Presentación en bolsa de polipropileno transparente con orificio para ganchera.\n\nTamaño: Diámetro 7.1 x 22.5 cm.\nColores: Blanco-Semi transparente, Azul, Rojo, Naranjo, Amarillo, Verde, Negro, Magenta.\nCapacidad: 700 cc.\nPresentación: Bolsa de polipropileno transparente, con orificio para ganchera.\nMaterial: HDPE.\n\n*Mínimo 12 unidades*",
+    "category": [
+      "DEPORTES",
+      "DESTACADOS"
+    ],
+    "images": [
+      "M31-0.jpeg",
+      "M31-1.jpeg",
+      "M31-2.jpeg",
+      "M31-3.jpeg",
+      "M31-4.jpeg",
+      "M31-5.jpeg",
+      "M31-6.jpeg",
+      "M31-7.jpeg",
+      "M31-8.jpeg"
+    ]
+  },
+  {
+    "sku": "TICH48",
+    "name": "Brasero / Parrilla para Carbón o Leña",
+    "price": 74900,
+    "desc": "Brasero / Parrilla para Carbón o Leña, de Acero negro. Altura para cocinar: 31cm. Rejilla cromada para cocinar: 51 x 51 x 4mm. Rejilla para carbón: 34 x 34cm x 3.5mm. Tapa con rejilla: 52 x 52 x 8.5cm. Bowl para fuego: 57 x 57 x 11.5cm. Patas: 7 x 7 x29.5cm. Incluye Manual instructivo de armado en español.\n\nTamaño: 81 x 81 x 44 cm.\nColor: Negro.\nPeso: 12.5 kg.\nPresentación: Caja de cartón.\nMaterial: Acero inoxidable.",
+    "category": [
+      "DESTACADOS",
+      "INVIERNO"
+    ],
+    "images": [
+      "H48-1.jpeg",
+      "H48-2.jpeg",
+      "H48-3.jpeg",
+      "H48-4.jpeg"
+    ]
+  },
+  {
+    "sku": "TICH49",
+    "name": "Chimenea de Exterior para Carbón o Leña",
+    "price": 74900,
+    "desc": "Chimenea de Exterior, para Carbón o Leña, de Acero negro. Con 3 compartimentos: inferior para almacenaje de leña o carbón, medio para hacer fuego, superior para tiraje de humo. Fácil de armar y transportar. Incluye Manual instructivo de armado en español.\n\nTamaño: 40 x 40 x 120 cm.\nColor: Negro.\nSugerencia: Grabado Láser.\nPeso: 10.4 kg\nPresentación: Caja de cartón.\nMaterial: Acero inoxidable.",
+    "category": [
+      "DESTACADOS",
+      "INVIERNO"
+    ],
+    "images": [
+      "H49-1-900x900.jpg",
+      "H49-2-900x900.jpg",
+      "H49-3-900x900.jpg",
+      "H49-4-900x900.jpg"
+    ]
+  },
+  {
+    "sku": "TICH39",
+    "name": "Chimenea de Exterior para Carbón o Leña",
+    "price": 79900,
+    "desc": "Chimenea de Exterior, para Carbón o Leña, de Acero negro. Con 3 compartimentos: inferior para almacenaje de leña o carbón, medio para hacer fuego, superior para tiraje de humo. Fácil de armar y transportar.\n\nTamaño: Diámetro 45 x H 134 cm.\nPeso: 9.3 kg.\nColor: Negro.\nMaterial: Acero inoxidable.",
+    "category": [
+      "DESTACADOS",
+      "INVIERNO"
+    ],
+    "images": [
+      "H39-01.jpg",
+      "H39-02.jpg",
+      "H39-03.jpg"
+    ]
+  },
+  {
+    "sku": "TICA0",
+    "name": "Lanyard Porta-Celular Corto",
+    "price": 790,
+    "desc": "Lanyard Porta-Celular corto de poliéster liso negro, con terminación de eco-cuero negro + mosquetón metálico + placa porta-celular de PVC negro.\n\nTamaño: Lanyard: largo total 86 cm (doble * 43 cm) / ancho: 2cm Placa: 6 x 4 cm / Largo total: 54.7 cm.\nPeso: 10g.\nColores: Negro..\nSugerencia de Impresión: Serigrafía sobre el lanyard de poliéster.\nAncho Lanyard: 2 cm.\nMaterial: Poliéster, PU, Acero Inoxidable\n\n𝐌𝐢́𝐧𝐢𝐦𝐨 𝟏𝟐 𝐮𝐧𝐢𝐝𝐚𝐝𝐞𝐬.",
+    "category": [
+      "LANYARDS E IDENTIFICACIÓN",
+      "SERIGRAFíA"
+    ],
+    "images": [
+      "A0-001.jpg",
+      "A0-002.jpg",
+      "A0-003.jpg",
+      "A0-004.jpg",
+      "A0-005.jpg",
+      "A0-006.jpg"
+    ]
+  }
 ];
 myProducts.forEach(p=>{p._name=p.name.toLowerCase();p._sku=p.sku.toLowerCase();});
